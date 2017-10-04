072ef3e68599d4552ecc3e4a43a282804d4d0c1f
Jouni Mietola <jouni.mietola@gmail.com>
2017-10-04 23:40:30 +0300

    add socket listeners

--- sockets/base.js
+++ sockets/base.js
@@ -12,7 +12,39 @@ module.exports = function (io) {
     socket.join(room);
   });
 
+  // when the user disconnects.. perform this
+  socket.on('remove', function (data) {
 
+    console.log("REMOVE", data);
+    let value = storage.getItemSync('locationKey');
+
+    var locationArray = value.locations;
+    console.log(locationArray);
+    value.locations = locationArray.filter(function(i) {
+    	return i.lat != data.lat;
+    });
+
+    storage.setItemSync('locationKey', value);
+
+    socket.broadcast.to(data.id).emit('user left', "hip has left room");
+
+  });
+
+  socket.on('say to', function(data){
+    console.log("say to", data.id, data.msg);
+    socket.broadcast.to(data.id).emit('chat message', data.msg);
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('hipFound', function (data) {
+    console.log("hipFound", data.id, data.msg);
+    socket.broadcast.to(data.id).emit('hipMatch', data.msg);
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    console.log("DISCONNECT");
+  });
 
   });
  }
