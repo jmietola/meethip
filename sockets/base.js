6ee2950fd954c73bbbc4e342a6130c8f57718224
Jouni Mietola <jouni.mietola@gmail.com>
2017-10-04 23:29:42 +0300

    test

--- sockets/base.js
+++ sockets/base.js
@@ -2,23 +2,6 @@ var storage = require('node-persist');
 
 module.exports = function (io) {
 
-  // Chatroom
-
-  var numUsers = 0;
-
-  getDate = function() {
-
-  var date = new Date();
-
-  var hour = date.getHours();
-  hour = (hour < 10 ? "0" : "") + hour;
-
-  var min  = date.getMinutes();
-  min = (min < 10 ? "0" : "") + min;
-
-  return hour + ":" + min;
-  }
-
   io.on('connection', (socket) => {
   var addedUser = false;
 
@@ -29,39 +12,7 @@ module.exports = function (io) {
     socket.join(room);
   });
 
-  // when the user disconnects.. perform this
-  socket.on('remove', function (data) {
 
-    console.log("REMOVE", data);
-    let value = storage.getItemSync('locationKey');
-
-    var locationArray = value.locations;
-    console.log(locationArray);
-    value.locations = locationArray.filter(function(i) {
-    	return i.lat != data.lat;
-    });
-
-    storage.setItemSync('locationKey', value);
-
-    socket.broadcast.to(data.id).emit('user left', "hip has left room");
-
-  });
-
-  socket.on('say to', function(data){
-    console.log("say to", data.id, data.msg);
-    socket.broadcast.to(data.id).emit('chat message', data.msg);
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('hipFound', function (data) {
-    console.log("hipFound", data.id, data.msg);
-    socket.broadcast.to(data.id).emit('hipMatch', data.msg);
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    console.log("DISCONNECT");
-  });
 
   });
  }
